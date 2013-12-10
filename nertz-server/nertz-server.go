6db7612b8a7c502b7ca1581c1610828dcd2c2e13
Andrew Roetker <andrew.roetker@puppetlabs.com>
2013-12-10 10:45:53 -0800

    Make things compile! Can't define custom methods!

--- nertz-server/nertz-server.go
+++ nertz-server/nertz-server.go
@@ -7,119 +7,52 @@ import (
     "os"
     "strconv"
     "net/http"
-    "json"
+    "encoding/json"
     "github.com/ajroetker/nertz"
 )
 
-type Credentials struct {
-    Username string
-    Password string
-}
 
-func (g *nertz.Game) WriteScores() {
-    for scoreupdate := range <-g.ScoreChan {
-        g.Done++
-        g.Scores[scoreupdate.Player] -= scoreupdate.Value
-        if g.Done == len(g.Clients) {
-            close g.ScoreChan
-        }
-    }
-}
-
-func (c *nertz.Client) SendMessages() {
-    ok := true
-    for ok {
-        select {
-        case msg := <-c.Messages:
-            jsonMsg := map[string]string{ "Message" : msg }
-            err := websocket.JSON.Send(c.Conn, jsonMsg)
-        case arena, ok := <-c.Arenas:
-            if ok {
-                err :=  websocket.JSON.Send(c.Conn, arena)
-            } else {
-                //if the channel is closed it means the game is over!
-                jsonMsg := map[string]string{ "Message" : "Nertz" }
-                err :=  websocket.JSON.Send(c.Conn, jsonMsg)
-            }
-        }
-        if err != nil {
-            panic("JSON.Send: " + err.Error())
-        }
-    }
-}
-
-func (c *nertz.Client) GetCredentials() *Credentials {
-    c.Messages <- "Credentials"
-    var creds Credentials
-    err := websocket.JSON.Receive(c.Conn, &creds)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return &creds
-}
-
-func MakeMoveHandler(g *Game) func(w http.ResponseWriter, r *http.Request) {
+func MakeMoveHandler(g *nertz.Game) func(w http.ResponseWriter, r *http.Request) {
     return func(w http.ResponseWriter, r *http.Request) {
         var resp = make(map[string]bool)
-        if !g.Done {
-            data := new(Move)
-            dec := json.Decoder(r.Body)
+        if ! g.Started {
+            data := new(nertz.Move)
+            dec := json.NewDecoder(r.Body)
             dec.Decode(&data)
-            resp["Ok"] = g.MakeMove(m *Move)
+            resp["Ok"] = g.MakeMove(data)
             w.Header().Set("Content-Type", "application/json")
-            enc := json.Encoder(w)
+            enc := json.NewEncoder(w)
             enc.Encode(resp)
             return
         } else {
             resp["Ok"] = false
             w.Header().Set("Content-Type", "application/json")
-            enc := json.Encoder(w)
+            enc := json.NewEncoder(w)
             enc.Encode(resp)
             return
         }
     }
 }
 
-func (g *nertz.Game) WaitForEnd(c *nertz.Client) {
-    var hand nertz.Hand
-    err := websocket.JSON.Receive(c.Conn, &hand)
-    if err != nil {
-        log.Fatal(err)
-    }
-    if !g.Done {
-        if hand.IsNertz() {
-            g.GameOver <- 1
-        } else {
-            //thanks for playing quitter!
-        }
-    }
-    scoreupdate := map[string]int{ c.Name : hand.Nertzpile.Len() }
-    g.ScoreChan <- scoreupdate
-    <-g.ScoreChan
-    err :=  websocket.JSON.Send(c.Conn, g.Scoreboard)
-    if err != nil {
-        panic("JSON.Send: " + err.Error())
-    }
-}
-
 func MakeAcceptPlayers(g *nertz.Game) func(ws *websocket.Conn) {
     return func(ws *websocket.Conn) {
-        if !g.Started {
+        if ! g.Started {
             fmt.Fprintf(os.Stdout, "***Nertz server accepted a new player***\n")
 
-            client = g.NewClient(ws)
+            client := g.NewClient(ws)
             //get username :: client.Name = ??
-            creds := c.GetCredentials()
-            c.Name = creds.Username
+            creds := client.GetCredentials()
+            client.Name = creds.Username
 
             go client.SendMessages()
             g.WaitForEnd(client)
-            }
         } else {
-            err := websocket.JSON.Send(c.Conn, Request{"Sorry",)
+            jsonMsg := map[string]string{ "Message" : "Nertz" }
+            err :=  websocket.JSON.Send(ws, jsonMsg)
             if err != nil {
                 panic("JSON.Send: " + err.Error())
             }
+        }
     }
 }
 
@@ -135,17 +68,18 @@ func main() {
     }
     listenAt := fmt.Sprintf(":%v", port)
 
-    arena := make([]*nertz.Piles, 0, 24)
+    arena := new(nertz.Arena)
+    arena.Piles = make([]*nertz.Pile, 0, 24)
     game := nertz.NewGame()
     game.Arenas <- arena
-    go g.BroadcastMessages()
-    go g.WriteScores()
+    go game.BroadcastMessages()
+    go game.WriteScores()
 
     GameHandler := MakeAcceptPlayers(game)
-    MoveHandler := nertz.MakeMoveHandler(game)
+    MoveHandler := MakeMoveHandler(game)
 
     http.Handle("/", websocket.Handler(GameHandler))
-    http.Handle("/move", http.Handler(MoveHandler))
+    http.Handle("/move", http.HandlerFunc(MoveHandler))
     fmt.Fprintf(os.Stdout, "Nertz server listening on port %v...\n", port)
     err = http.ListenAndServe(listenAt, nil)
     if err != nil {
--- nertz.go
+++ nertz.go
@@ -1,8 +1,9 @@
 package nertz
 
 import (
-    "log"
     "container/list"
+    "log"
+    websocket "code.google.com/p/go.net/websocket"
 )
 
 type Card struct{
@@ -23,20 +24,20 @@ type Game struct {
     Arenas chan *Arena
     Updates chan *Arena
     NewClients chan *Client
-    ScoreChan chan map[string]int
+    ScoreChan chan map[string]interface{}
     GameOver chan int
     Started bool
     Done int
     Scoreboard map[string]int
 }
 
-func NewGame() {
+func NewGame() *Game {
     var game *Game  = new(Game)
     game.Clients    = make([]*Client, 0, 6)
     game.Arenas     = make(chan *Arena)
     game.Updates    = make(chan *Arena, 10)
     game.NewClients = make(chan *Client, 6)
-    game.ScoreChan  = make(chan map[string]int, 6)
+    game.ScoreChan  = make(chan map[string]interface{}, 6)
     game.GameOver   = make(chan int, 6)
     game.Started    = false
     game.Done       = 0
@@ -59,8 +60,8 @@ type Client struct {
 }
 
 func (g *Game) AddNewClients() {
-    for cli := range <-g.NewClients {
-        g.Clients[len(cli)] = cli
+    for cli := range g.NewClients {
+        g.Clients[len(g.Clients)] = cli
     }
 }
 
@@ -77,7 +78,7 @@ func (g *Game) TallyUp() {
     a := <-g.Arenas
     var scores map[string]int
     for _, pile := range a.Piles {
-        for _, card := range a.Cards {
+        for _, card := range pile.Cards {
             scores[card.Player]++
         }
     }
@@ -85,37 +86,120 @@ func (g *Game) TallyUp() {
 }
 
 func (g *Game) BroadcastMessages() {
-    for ! g.Done {
+    for {
         select {
         case arena := <-g.Updates:
             for _, c := range g.Clients {
                 c.Arenas <- arena
             }
         case <-g.GameOver:
-            g.Done = true
             g.TallyUp()
             close(g.Arenas)
             for _, c := range g.Clients {
                 close(c.Arenas)
             }
+            return
         }
     }
 }
 
-func (s *Game) MakeMove(m *Move) bool {
+func (g *Game) WaitForEnd(c *Client) {
+    var hand Hand
+    err := websocket.JSON.Receive(c.Conn, &hand)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if g.Done == 0  {
+        if hand.IsNertz() {
+            g.GameOver <- 1
+        } else {
+
+            scoreupdate := map[string]interface{}{
+                "Player" : c.Name,
+                "Value"  : hand.Nertzpile.Len(),
+            }
+
+            g.ScoreChan <- scoreupdate
+            return
+            //thanks for playing quitter!
+        }
+    }
+    scoreupdate := map[string]interface{}{
+        "Player" : c.Name,
+        "Value"  : hand.Nertzpile.Len(),
+    }
+    g.ScoreChan <- scoreupdate
+    <-g.ScoreChan
+    err = websocket.JSON.Send(c.Conn, g.Scoreboard)
+    if err != nil {
+        panic("JSON.Send: " + err.Error())
+    }
+}
+
+type Credentials struct {
+    Username string
+    Password string
+}
+
+func (g *Game) WriteScores() {
+    <-g.GameOver
+    for scoreupdate := range g.ScoreChan {
+        g.Done++
+        g.Scoreboard[scoreupdate["Player"].(string)] -= scoreupdate["Value"].(int)
+        if g.Done == len(g.Clients) {
+            close(g.ScoreChan)
+        }
+    }
+}
+
+func (c *Client) SendMessages() {
+    ok := true
+    var err error
+    for ok {
+        select {
+        case msg := <-c.Messages:
+            jsonMsg := map[string]string{ "Message" : msg }
+            err = websocket.JSON.Send(c.Conn, jsonMsg)
+        case arena, ok := <-c.Arenas:
+            if ok {
+                err =  websocket.JSON.Send(c.Conn, arena)
+            } else {
+                //if the channel is closed it means the game is over!
+                jsonMsg := map[string]string{ "Message" : "Nertz" }
+                err =  websocket.JSON.Send(c.Conn, jsonMsg)
+            }
+        }
+        if err != nil {
+            panic("JSON.Send: " + err.Error())
+        }
+    }
+}
+
+func (c *Client) GetCredentials() *Credentials {
+    c.Messages <- "Credentials"
+    var creds Credentials
+    err := websocket.JSON.Receive(c.Conn, &creds)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return &creds
+}
+
+func (s *Game) MakeMove(move *Move) bool {
     a := <-s.Arenas
     size := len(a.Piles[move.Pile].Cards)
+    var resp bool
     if size == 0 && move.Card.Value == 1 {
         a.Piles[move.Pile].Cards[size] = move.Card
-        resp := true
+        resp = true
     } else {
-        top := a.Piles[move.Pile].Cards[size-1].value
+        top := a.Piles[move.Pile].Cards[size-1].Value
         suit := a.Piles[move.Pile].Cards[0].Suit
         if move.Card.Value != top + 1 || suit != move.Card.Value || top == 13 {
-            resp := false
+            resp = false
         } else {
             a.Piles[move.Pile].Cards[size] = move.Card
-            resp := true
+            resp = true
         }
     }
     s.Arenas <- a
@@ -134,7 +218,7 @@ type Hand struct {
 }
 
 func (h *Hand) IsNertz() bool {
-    return Nertzpile.Len() == 0
+    return h.Nertzpile.Len() == 0
 }
 
 type PlayerMove struct {
@@ -143,8 +227,9 @@ type PlayerMove struct {
     Cards *list.List
 }
 
+/*
 func (h *Hand) MakeMoves() {
-    for m := range <-h.Moves {
+    for m := range h.Moves {
         
     }
 
@@ -170,3 +255,4 @@ func (h *Hand) Valid(m *PlayerMove) bool {
          return false
     }
 }
+*/
